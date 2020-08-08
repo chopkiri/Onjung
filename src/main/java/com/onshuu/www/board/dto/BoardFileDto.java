8ec4a93a3ea396107ed6984f92d76c54bbd89e68
또띠니-콤푸타\장소진 <sojin4587@gmail.com>
2020-08-08 16:49:34 +0900

    no message

---
+++ src/main/java/com/onshuu/www/board/dto/BoardFileDto.java
@@ -0,0 +1,10 @@
+package com.onshuu.www.board.dto;
+
+public class BoardFileDto {
+
+    private int idx;
+    private int boardIdx;
+    private String originalFileName;
+    private String storedFilePath;
+    private long fileSize;
+}
